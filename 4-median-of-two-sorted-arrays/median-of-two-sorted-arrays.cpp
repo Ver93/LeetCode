6334b831e08c34e90819cffa55262dba28fbf450
VictorRotman <126388506+Ver93@users.noreply.github.com>
2025-02-18 09:19:59 +0100

    Update median-of-two-sorted-arrays.cpp

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,7 +4,7 @@ public:
             
         std::vector<int> merged(nums1.size() + nums2.size());
 
-        std::merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), merged.begin()); // Easy mode?
+        std::merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), merged.begin());
 
         int mid = merged.size()/2;
         double r;
